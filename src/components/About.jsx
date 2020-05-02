21501ee789ce8514fdd2c41273c7759ec4f66ad6
costolo <ben.costolo@gmail.com>
2020-05-02 13:16:28 -0400

    Initial commit/add recorder route

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Subcomponent from './Subcomponent';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import Subcomponent from "./Subcomponent";
+import { Route, Switch } from "react-router-dom";
 
 const About = (props) => (
-  <div className='about'>
+  <div className="about">
     About
     <Switch>
-      <Route path='/about/subroute' component={Subcomponent} />
+      <Route path="/about/subroute" component={Subcomponent} />
     </Switch>
   </div>
-)
+);
 
 export default About;
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import App from "./components/App";
+import Home from "./components/Home";
+import About from "./components/About";
+import Recorder from "./components/Recorder";
 
 const routes = (
   <App>
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/recorder" component={Recorder} />
     </Switch>
   </App>
-)
+);
 
 export { routes };
