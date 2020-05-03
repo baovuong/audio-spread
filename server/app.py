146f87ade0b44bfcd1435361d09bb3f7843f301c
Bao Vuong <vqbao@lv5.org>
2020-05-02 23:35:05 -0400

    upload works and gives some sort of output

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
-'''server/app.py - main api app declaration'''
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
-'''Main wrapper for app creation'''
 app = Flask(__name__, static_folder='../build')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 CORS(app)
 
 ##
@@ -25,3 +24,26 @@ def index(path):
   '''Return index.html for all non-api routes'''
   #pylint: disable=unused-argument
   return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+  
+  if 'file' not in request.files:
+    resp = jsonify({'message': 'No file part in the request'})
+    resp.status_code = 400
+    return resp
+  
+  file = request.files['file']
+  if file.filename == '':
+    resp = jsonify({'message': 'No file selected for uploading'})
+    resp.status_code = 400
+    return resp 
+
+  
+  # process file
+  return jsonify({
+    'filename': file.filename,
+    'mimetype': file.mimetype
+  })
+  
+  
