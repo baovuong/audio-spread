d3639c8bcaaffbb59bd68f225c1dcef14e0e1f8e
Bao Vuong <vqbao@lv5.org>
2020-05-10 02:06:37 -0400

    Feature/backend (#9)
    * test
    
    * upload works and gives some sort of output
    
    * ignoring package-lock
    
    * cleaned up code. audio file check
    
    * getting redis to work
    
    * got logging and redis to work
    
    * trying to get clamav going
    
    * removing mongo
    
    * logging into file
    
    * got dropbox upload to work
    
    * fixed virus scanning
    
    * moving clamav and dropbox to global

--- README.md
+++ README.md
@@ -13,11 +13,13 @@ Simple boilerplate for a Flask backend and React client including:
 
 ## Dependencies
 
-To install the boilerplate dependencies, you can run:
+Install the following:
+* clamav
+* redis
+
+Run the following:
 
 ```bash
-git clone https://github.com/YaleDHLab/flask-react-boilerplate
-cd flask-react-boilerplate
 npm install --no-optional
 pip install -r requirements.txt
 ```
--- server/app.py
+++ server/app.py
@@ -1,19 +1,139 @@
-'''server/app.py - main api app declaration'''
-from flask import Flask, jsonify, send_from_directory
+import os 
+import ntpath 
+import mimetypes
+import dropbox 
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
-'''Main wrapper for app creation'''
+from logging.config import dictConfig
+import clamd
+
+from werkzeug.utils import secure_filename
+
+from rq import Queue
+from rq.job import Job
+from worker import conn
+from config_variables import config
+
+
+# config
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'filename': 'audio-spread.log',
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
 app = Flask(__name__, static_folder='../build')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = 'upload_tmp/'
 CORS(app)
+
+app.logger.info('connecting to Redis queue')
+q = Queue(connection=conn)
+
+# clamd init
+app.logger.info('initializing Clamav')
+try:
+  cd = clamd.ClamdUnixSocket()
+  cd.ping()
+except:
+  app.logger.error('could not open clamd unix socket. attemping network socket')
+  cd = clamd.ClamdNetworkSocket()
+  try:
+    cd.ping()
+  except clamd.ConnectionError:
+    app.logger.error('could not connect to clamd server either by unix or network socket. go big or go home I guess.')
+    cd = None 
+
+# dropbox init
+app.logger.info('initializing Dropbox')
+dbx = dropbox.Dropbox(config['DROPBOX_ACCESS_TOKEN'])
+
+def isaudio(file):
+  return 'audio/' in file.mimetype
+
+def has_virus(file_path):
+  if cd is None:
+    return False 
+  app.logger.info('scanning for viruses')
+  result = cd.scan(os.path.abspath(file_path))
+  app.logger.info('scanned for viruses')
+  app.logger.info('scan results: %s, %s' % (result[os.path.abspath(file_path)][0], result[os.path.abspath(file_path)][1]))
+  return result[os.path.abspath(file_path)][1] is not None 
+
+def errormessage(message, status_code):
+  resp = jsonify({'message': message})
+  resp.status_code = status_code
+  return resp 
+
+def save_to_dropbox(file_path):
+  # TODO work on this
+  app.logger.info('saving to dropbox')
+  with open(os.path.abspath(file_path), 'rb') as file:
+    contents = file.read()
+    dbx.files_upload(contents, '/' + ntpath.basename(file_path))
+
+
+def process_file(file_path):
+  app.logger.info('processing %s' % ntpath.basename(file_path))
+  
+  if not has_virus(file_path):
+    save_to_dropbox(file_path)
+  else:
+    app.logger.info('%s is suspected to be a virus. deleting' % ntpath.basename(file_path))
 
 ##
 # API routes
 ##
+@app.route('/api/upload', methods=['POST'])
+def upload():
+  
+  name = request.args.get('name', 'unknown')
+  email = request.args.get('email', 'unknown')
 
-@app.route('/api/items')
-def items():
-  '''Sample API route for data'''
-  return jsonify([{'title': 'A'}, {'title': 'B'}])
+  if 'file' not in request.files:
+    app.logger.error('No file part in the request')
+    return errormessage('No file part in the request', 400)
+  
+  file = request.files['file']
+  if file.filename == '':
+    app.logger.error('No file selected for uploading')
+    return errormessage('No file selected for uploading', 400) 
+
+  if not isaudio(file):
+    app.logger.error('file %s has mimetype %s. not audio' % (file.filename, file.mimetype))
+    return errormessage('not audio', 400)
+  
+
+  # save file to directory
+  filename = name + '-' + email + '-' + secure_filename(file.filename)
+  upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+  file.save(upload_path)
+
+  # process file
+  from app import process_file
+
+  job = q.enqueue_call(
+    func=process_file, args=(upload_path,), result_ttl=5000
+  )
+  print(job.get_id())
+
+  return jsonify({
+    'message': 'Uploaded successfully.'
+  })
+
 
 ##
 # View route
@@ -25,3 +145,7 @@ def index(path):
   '''Return index.html for all non-api routes'''
   #pylint: disable=unused-argument
   return send_from_directory(app.static_folder, 'index.html')
+
+
+  
+  
